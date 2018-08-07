8635b41351cc59f09876d8e2e40c1c900f380342
Teo Sin Yan <16033774@myrp.edu.sg>
2018-08-07 22:28:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16033774/p12_mydatabook/BioFragment.java
@@ -0,0 +1,35 @@
+package com.example.a16033774.p12_mydatabook;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BioFragment extends Fragment {
+
+    Button btnEdit;
+
+    public BioFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bio, container, false);
+
+    }
+
+
+}
